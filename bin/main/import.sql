08f1c50710beaf29ea15a1dd83116bc47913b289
rhayeksa <rhayeksads@gmail.com>
2020-08-13 16:59:43 +0700

    delete bin

--- bin/main/import.sql
+++
@@ -1,8 +0,0 @@
-insert into siswa (id, created_at, updated_at, nisn, nama, kelas, gender, email, hp, alamat) values (1, '2020-03-04 00:00:00.000000', null, '98889988898', 'siswa_1', '7.4', 'laki-laki', 'siswa_1@email.com', '089512341234', 'Jl.merbabu kav');
-insert into siswa (id, created_at, updated_at, nisn, nama, kelas, gender, email, hp, alamat) values (2, '2020-03-04 00:00:00.000000', null, '78787878787', 'siswa_2', '7.4', 'perempuan', 'siswa_2@email.com', '081212344321', 'Jl.menuju sukses');
-insert into siswa (id, created_at, updated_at, nisn, nama, kelas, gender, email, hp, alamat) values (3, '2020-03-04 00:00:00.000000', null, '57575757575', 'siswa_3', '9.8', 'laki-laki', 'siswa_3@email.com', '083843215535', 'Jl.bercabang');
-
-insert into guru (id, created_at, updated_at, nign, nama, gender, email, hp, alamat) values (1, '2020-03-04 00:00:00.000000', null, '50000000000', 'guru_1', 'laki-laki', 'guru_1@email.com', '089612344321', 'Jl.berantai tembaga');
-insert into guru (id, created_at, updated_at, nign, nama, gender, email, hp, alamat) values (2, '2020-03-04 00:00:00.000000', null, '60000000000', 'guru_2', 'laki-laki', 'guru_2@email.com', '087842343243', 'Jl.penembak jitu');
-insert into guru (id, created_at, updated_at, nign, nama, gender, email, hp, alamat) values (3, '2020-03-04 00:00:00.000000', null, '70000000000', 'guru_3', 'perempuan', 'guru_3@email.com', '085312345213', 'Jl.pendekar pedang');
-
